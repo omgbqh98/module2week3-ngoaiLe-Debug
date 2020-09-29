03adca71e15c476fd3017793ec0a60538f5030e9
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-09-29 11:20:01 +0700

    sdf

---
+++ BT-batLoi-LopIllegalTriangleException/src/IllegalTriangleException.java
@@ -0,0 +1,27 @@
+import java.util.EmptyStackException;
+import java.util.Scanner;
+
+public class IllegalTriangleException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int canh1 , canh2, canh3;
+
+
+                System.out.println("input canh 1");
+                canh1 = scanner.nextInt();
+                System.out.println("input canh 2");
+                canh2 = scanner.nextInt();
+                System.out.println("input canh 3");
+                canh3 = scanner.nextInt();
+        try {
+            if (canh1 > 0 && canh2 > 0 && canh3 > 0 && canh1 + canh2 > canh3 && canh1 + canh3 > canh2) {
+                System.out.println("c1= " + canh1 + ", c2= " + canh2 + ", c3= " + canh3);
+            } else {
+                throw new EmptyStackException();
+            }
+
+        } catch (Exception e) {
+            System.out.println("loi rooooi");
+        }
+                }
+        }
